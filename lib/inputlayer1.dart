8df57e3b6d388193ef2f10d474dffcd91de0775e
suneemon1 <suneemon.com@gmail.com>
2023-07-29 11:43:54 +0900

    2023-07-29

---
+++ lib/inputlayer1.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
+import 'dart:ui';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:myapp/utils.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,16 @@ import 'package:flutter/gestures.dart';
 import 'dart:ui';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:myapp/utils.dart';
-// import 'package:myapp/page-1/result.dart';
-// import 'package:myapp/page-1/waiting.dart';
-// import 'package:myapp/page-1/outputlayer.dart';
-// import 'package:myapp/page-1/inputlayer2.dart';
+import 'package:myapp/page-1/result.dart';
+import 'package:myapp/page-1/waiting.dart';
+import 'package:myapp/page-1/outputlayer.dart';
+import 'package:myapp/page-1/inputlayer2.dart';
 import 'package:myapp/page-1/inputlayer1.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
